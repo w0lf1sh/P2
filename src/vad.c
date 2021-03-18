680a26c55eccc81c9748052bcdfdedc020f44c23
0loyalty <claptor@DESKTOP-PVP20BQ.localdomain>
2021-03-18 11:04:04 +0100

    funciones P1 implemntadas a vad.c

--- src/vad.c
+++ src/vad.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "pav_analysis.h"
 #include "vad.h"
 
 const float FRAME_TIME = 10.0F; /* in ms. */
@@ -42,7 +43,11 @@ Features compute_features(const float *x, int N) {
    * For the moment, compute random value between 0 and 1 
    */
   Features feat;
-  feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  feat.zcr = compute_zcr(x,N,16000);
+  feat.p = compute_power(x,N);
+  feat.am = compute_am(x,N);
+  
+
   return feat;
 }
 
